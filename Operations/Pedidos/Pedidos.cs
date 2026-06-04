7e8533daecd9e1eac1ef14e572a797f60629cf8c
Garyjnm <garyn7674@gmail.com>
2026-06-03 19:41:38 -0600

    Creando el modelo de los pedidos

---
+++ Operations/Pedidos/Pedidos.cs
@@ -0,0 +1,23 @@
+using APPCORE;
+using BusinessLogic.Connection;
+using System;
+
+namespace Operations.Pedidos
+{
+    public class Pedidos : EntityClass
+    {
+        [PrimaryKey(Identity = true)]
+        public int? id_pedido { get; set; }
+        public int? id_cliente { get; set; }
+        public int? id_repartidor { get; set; }
+        public int? id_categoria { get; set; }
+        public int? id_estado { get; set; }
+        public decimal monto_envio { get; set; }
+        public DateTime? fecha_hora { get; set; }
+
+        public Pedidos()
+        {
+            this.MDataMapper = new BDConnection().DBOrigen;
+        }
+    }
+}
