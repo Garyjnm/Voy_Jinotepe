9e36510665b1d14e703331a8a71a62057554fa15
Garyjnm <garyn7674@gmail.com>
2026-06-03 19:44:01 -0600

    Creando el archivo para la analitica

---
+++ ETLService/Controllers/AnalyticsController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using Operations.Pedidos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ETLService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AnalyticsController : ControllerBase
+    {
+        
+    }
+}
